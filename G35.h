1eb81346f7120364fad258be255bbe4ba95b0c34
Mike Tsao <mike@sowbug.com>
2011-11-16 21:19:54 -0800

    New.

---
+++ G35.h
@@ -0,0 +1,66 @@
+/*
+  G35: An Arduino library for GE Color Effects G-35 holiday lights.
+  Copyright © 2011 The G35 Authors. Use, modification, and distribution are
+  subject to the BSD license as described in the accompanying LICENSE file.
+
+  Original version by Paul Martis (http://www.digitalmisery.com). See
+  README for complete attributions.
+*/
+
+#ifndef INCLUDE_G35_ARDUINO_H
+#define INCLUDE_G35_ARDUINO_H
+
+#include <WProgram.h>
+
+#define color_t uint16_t
+#define CHANNEL_MAX           (0xF)   // Each color channel is 4-bit
+#define DEFAULT_INTENSITY     (0xCC)  // Brightness is 8-bit
+#define HUE_MAX               ((CHANNEL_MAX+1)*6-1)
+
+// Color is 12-bit (4-bit each R, G, B)
+#define COLOR(r,g,b)          ((r)+((g)<<4)+((b)<<8))
+#define COLOR_WHITE           COLOR(CHANNEL_MAX,CHANNEL_MAX,CHANNEL_MAX)
+#define COLOR_BLACK           COLOR(0,0,0)
+#define COLOR_RED             COLOR(CHANNEL_MAX,0,0)
+#define COLOR_GREEN           COLOR(0,CHANNEL_MAX,0)
+#define COLOR_BLUE            COLOR(0,0,CHANNEL_MAX)
+#define COLOR_CYAN            COLOR(0,CHANNEL_MAX,CHANNEL_MAX)
+#define COLOR_MAGENTA         COLOR(CHANNEL_MAX,0,CHANNEL_MAX)
+#define COLOR_YELLOW          COLOR(CHANNEL_MAX,CHANNEL_MAX,0)
+#define COLOR_PURPLE          COLOR(0xa,0x3,0xd)
+#define COLOR_ORANGE          COLOR(0xf,0x1,0x0)
+#define COLOR_WARMWHITE       COLOR(0xf,0x7,0x2)
+
+class G35 {
+ public:
+  // Arduino Pin # and total lights on string
+  G35(int pin, int light_count);
+
+  // Turn on a specific LED
+  void set_color(uint8_t led, uint8_t intensity, color_t color);
+
+  // Color data type
+  color_t color(uint8_t r, uint8_t g, uint8_t b);
+
+  color_t color_hue(uint8_t h);
+
+  void fill_color(uint8_t begin, uint8_t count, uint8_t intensity,
+                  color_t color);
+
+  // Make all LEDs the same color
+  void fill_color_same(uint8_t begin, uint8_t count, uint8_t intensity,
+                       color_t color);
+
+  // Need to enumerate in order to address individual LEDs
+  void enumerate();
+ private:
+  // Low-level one-wire protocol commands
+  void begin();
+  void one();
+  void zero();
+  void end();
+  int _pin;
+  int _light_count;
+};
+
+#endif  // INCLUDE_G35_ARDUINO_H
---
+++ examples/BasicExample/BasicExample.pde
@@ -0,0 +1,37 @@
+#include <G35.h>
+
+#define OUT_PIN 19      // Arduino pin #
+#define LIGHT_COUNT 50  // Total # of lights on string (usually 50, 48, or 36)
+
+static uint16_t c;
+
+G35 lights(OUT_PIN, LIGHT_COUNT);  // Constructor
+
+void setup() {
+  // Enumerate lights on string to enable individual bulb addressing
+  lights.enumerate();
+}
+
+void loop() {
+  // fill_color makes all bulbs from 0 to LIGHT_COUNT the same brightness
+  // and color
+  lights.fill_color(0, LIGHT_COUNT, DEFAULT_INTENSITY, COLOR_RED);
+
+  delay(1000);
+
+  lights.fill_color(0, LIGHT_COUNT, DEFAULT_INTENSITY, COLOR_GREEN);
+
+  delay(1000);
+
+  // set_color addresses one specific bulb at a time with brightness and
+  // color setting
+  for (int i = 0; i < LIGHT_COUNT / 2; ++i) {
+    color_t color = lights.color_hue((i + c) % (HUE_MAX + 1));
+    lights.set_color(i, DEFAULT_INTENSITY, color);
+    lights.set_color(LIGHT_COUNT - 1 - i, DEFAULT_INTENSITY, color);
+  }
+
+  ++c;
+
+  delay(1000);
+}
