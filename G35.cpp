a4aaa36d908816bb1fdec75694d3f4f9f27233f7
Mike Tsao <mike@sowbug.com>
2011-12-15 22:07:40 -0800

    Mo betta test patterns. Fix indents.

--- G35.cpp
+++ G35.cpp
@@ -29,14 +29,14 @@ G35::G35(int pin, int light_count) {
 #endif
 #define DELAYEND 40     // should be ~ 30uS long
 
-#define ZERO(x) digitalWrite(x, LOW); \
-  delayMicroseconds(DELAYSHORT); \
-  digitalWrite(x, HIGH); \
+#define ZERO(x) digitalWrite(x, LOW);           \
+  delayMicroseconds(DELAYSHORT);                \
+  digitalWrite(x, HIGH);                        \
   delayMicroseconds(DELAYLONG);
 
-#define ONE(x) digitalWrite(x, LOW); \
-  delayMicroseconds(DELAYLONG); \
-  digitalWrite(x, HIGH); \
+#define ONE(x) digitalWrite(x, LOW);            \
+  delayMicroseconds(DELAYLONG);                 \
+  digitalWrite(x, HIGH);                        \
   delayMicroseconds(DELAYSHORT);
 
 void G35::set_color(uint8_t led, uint8_t intensity, color_t color) {
@@ -89,7 +89,7 @@ void G35::set_color(uint8_t led, uint8_t intensity, color_t color) {
 }
 
 bool G35::set_color_if_in_range(uint8_t position, uint8_t intensity,
-				color_t color) {
+                                color_t color) {
   if (position >= light_count_) {
     return false;
   }
@@ -116,7 +116,7 @@ color_t G35::color_hue(uint8_t h) {
 }
 
 void G35::fill_color(uint8_t begin, uint8_t count,
-		     uint8_t intensity, color_t color) {
+                     uint8_t intensity, color_t color) {
   while (count--) {
     set_color(begin++, intensity, color);
   }
@@ -129,25 +129,25 @@ void G35::fill_random_max(uint8_t begin, uint8_t count, uint8_t intensity) {
 }
 
 void G35::fill_sequence(uint8_t begin, uint8_t count,
-			uint16_t sequence, uint8_t span_size,
-			uint8_t intensity,
-			color_t (*sequence_func)(uint16_t sequence)) {
+                        uint16_t sequence, uint8_t span_size,
+                        uint8_t intensity,
+                        color_t (*sequence_func)(uint16_t sequence)) {
   while (count--) {
     set_color(begin + count, intensity,
-	      sequence_func(sequence++ / span_size));
+              sequence_func(sequence++ / span_size));
   }
 }
 
 void G35::fill_sequence(uint16_t sequence, uint8_t span_size,
-			uint8_t intensity,
-			color_t (*sequence_func)(uint16_t sequence)) {
+                        uint8_t intensity,
+                        color_t (*sequence_func)(uint16_t sequence)) {
   fill_sequence(0, light_count_, sequence, span_size, intensity, sequence_func);
 }
 
 void G35::fill_sequence(uint8_t begin, uint8_t count,
-			uint16_t sequence, uint8_t span_size,
-			bool (*sequence_func)(uint16_t sequence, color_t& color,
-					      uint8_t& intensity)) {
+                        uint16_t sequence, uint8_t span_size,
+                        bool (*sequence_func)(uint16_t sequence, color_t& color,
+                                              uint8_t& intensity)) {
   while (count--) {
     color_t color;
     uint8_t intensity;
@@ -175,15 +175,22 @@ void G35::enumerate_reverse() {
 }
 
 void G35::test_patterns() {
-  fill_color(0, light_count_, 0, rainbow_color(RB_FIRST));
+  const uint8_t d = 1000 / light_count_;
+  const uint8_t last_light = light_count_ - 1;
+  fill_sequence(0, light_count_, 0, 1, 0, rainbow_color);
   fade_in(1);
-  for (uint8_t i = RB_FIRST; i <= RB_LAST; ++i) {
-    delay(50);
-    fill_color(0, light_count_, MAX_INTENSITY, rainbow_color(i));
-  }
-  fade_out(1);
-  fill_color(0, light_count_, MAX_INTENSITY, COLOR_BLACK);
   delay(500);
+  fade_out(1);
+  for (uint8_t i = 0; i < light_count_; ++i) {
+    set_color(i, MAX_INTENSITY, COLOR_GREEN);
+    set_color(last_light - i, MAX_INTENSITY, COLOR_BLUE);
+    if (i > 0) {
+      set_color(i - 1, MAX_INTENSITY, COLOR_BLACK);
+      set_color(last_light - i + 1, MAX_INTENSITY, COLOR_BLACK);
+    }
+    delay(d);
+  }
+  fill_color(0, light_count_, MAX_INTENSITY, COLOR_BLACK);
 }
 
 color_t G35::rainbow_color(uint16_t color) {
